8fbb5911ead944c436f776ef19ef9bfe36b306df
Ashmit Das <dasashmit16@gmail.com>
2023-10-21 19:15:47 +0530

    This is a longer approach

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,14 +1,20 @@
 public class SquareRoot {
     public static void main(String[] args) {
-        System.out.println(root(2147395600));
+        System.out.println(root(1978959248));
 
     }
 
-    static int root(int x) {
-        double i = 1;
-        while (i * i <= x) {
-            i += 1;
+    // static int root(int x) {
+    // double i = 1;
+    // while (i * i <= x) {
+    // i += 1;
+    // }
+    // return (int) (i - 1);
+    // }
+    static int root(double x) {
+        double j;
+        for (j = x / 2; j * j > x; j--) {
         }
-        return (int) (i - 1);
+        return x == 1 ? 1 : (int) j;
     }
 }
