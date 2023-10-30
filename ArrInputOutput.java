a9936a66c70abc528abc58694d51d96fc82edf78
Ashmit Das <dasashmit16@gmail.com>
2023-10-30 19:56:00 +0530

    Simple input output of array in java

---
+++ ArrInputOutput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrInputOutput {
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < myArr.length; i++) {
+
+            System.out.println("Enter the data of the " + (i + 1) + " node: ");
+            myArr[i] = input.nextInt();
+
+        }
+        for (int i : myArr) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
