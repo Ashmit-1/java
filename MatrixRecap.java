b169018fa2616f925c74cc9e0e963adf2873a966
Ashmit Das <dasashmit16@gmail.com>
2023-11-18 23:10:08 +0530

    Modifications in display

--- MatrixRecap.java
+++ MatrixRecap.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class MatrixRecap {
     public static void main(String[] args) {
         int[][] matrix1 = input_mat();
-        int[][] matrix2 = input_mat();
+        // int[][] matrix2 = input_mat();
         display_mat(matrix1);
-        display_mat(matrix2);
-        System.out.println("The sum of the 2 matrices is: ");
-        display_mat(mat_add(matrix1, matrix2));
-        System.out.println("The product of the 2 matrices is: ");
-        display_mat(mat_mul(matrix1, matrix2));
+        // display_mat(matrix2);
+        // System.out.println("The sum of the 2 matrices is: ");
+        // display_mat(mat_add(matrix1, matrix2));
+        // System.out.println("The product of the 2 matrices is: ");
+        // display_mat(mat_mul(matrix1, matrix2));
 
     }
 
@@ -34,44 +34,51 @@ public class MatrixRecap {
     }
 
     static void display_mat(int[][] x) {
-        for (int rows = 0; rows < x.length; rows++) {
-            for (int cols = 0; cols < x[rows].length; cols++) {
-                System.out.print(x[rows][cols] + " ");
+        // for (int rows = 0; rows < x.length; rows++) {
+        // for (int cols = 0; cols < x[rows].length; cols++) {
+        // System.out.print(x[rows][cols] + " ");
+
+        // }
+        // System.out.println();
+        // }
+        // System.out.println();
+        for (int i = 0; i < x.length; i++) {
+            for (int is : x[i]) {
+                System.out.print(is + " ");
 
             }
             System.out.println();
         }
-        System.out.println();
     }
 
-    static int[][] mat_add(int[][] x, int[][] y) {
-        int[][] sum = new int[x.length][x[0].length];
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x[i].length; j++) {
-                sum[i][j] = x[i][j] + y[i][j];
-            }
+    // static int[][] mat_add(int[][] x, int[][] y) {
+    // int[][] sum = new int[x.length][x[0].length];
+    // for (int i = 0; i < x.length; i++) {
+    // for (int j = 0; j < x[i].length; j++) {
+    // sum[i][j] = x[i][j] + y[i][j];
+    // }
 
-        }
-        return sum;
-    }
+    // }
+    // return sum;
+    // }
 
-    static int[][] mat_mul(int[][] x, int[][] y) {
-        int[][] product = new int[x.length][y[0].length];
-        for (int i = 0; i < product.length; i++) {
-            for (int j = 0; j < product[i].length; j++) {
-                product[i][j] = 0;
+    // static int[][] mat_mul(int[][] x, int[][] y) {
+    // int[][] product = new int[x.length][y[0].length];
+    // for (int i = 0; i < product.length; i++) {
+    // for (int j = 0; j < product[i].length; j++) {
+    // product[i][j] = 0;
 
-            }
-        }
-        for (int i = 0; i < product.length; i++) {
-            for (int j = 0; j < product[i].length; j++) {
-                for (int k = 0; k < product.length; k++) {
-                    product[k][i] += x[k][j] * y[j][i];
-                }
+    // }
+    // }
+    // for (int i = 0; i < product.length; i++) {
+    // for (int j = 0; j < product[i].length; j++) {
+    // for (int k = 0; k < product.length; k++) {
+    // product[k][i] += x[k][j] * y[j][i];
+    // }
 
-            }
-        }
-        return product;
-    }
+    // }
+    // }
+    // return product;
+    // }
 
 }
